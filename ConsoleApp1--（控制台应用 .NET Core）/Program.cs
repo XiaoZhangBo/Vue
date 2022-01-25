f851c10f185609184c077252fa92b927d1b85d22
肖张博 <98293312+XiaoZhangBo@users.noreply.github.com>
2022-01-25 09:44:27 +0800

    Add files via upload
    hhhhhhhh

---
+++ ConsoleApp1--（控制台应用 .NET Core）/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1___控制台应用_.NET_Core_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
